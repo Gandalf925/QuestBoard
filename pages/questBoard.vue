eab48f7c360f965b7cf77748f84ae7288e34fa3e
Gandalf925 <theengineerofoz@gmail.com>
2022-07-28 20:32:19 +0900

    add/plugin/handCashConnect.js

---
+++ pages/questBoard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    MAIN
+    <v-btn @click="login()">HandCash login</v-btn>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    login() {
+      const redirectionLoginUrl = this.$handCash.getRedirectionUrl()
+      console.log(redirectionLoginUrl)
+    },
+  },
+}
+</script>
+
+<style></style>
